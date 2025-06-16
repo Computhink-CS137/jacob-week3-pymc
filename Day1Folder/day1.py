6c3671b7a1517d34cd229352431aaba63a840152
Computhink-CS137 <cs137@computhink.com.sg>
2025-06-16 04:37:05 +0000

    Jun 16, 2025, 4:37 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,40 +1,84 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+print("Hello from day1")
 
+# print("my name is jacob")
+# print("I'm 14 years old")
+# print("I study in SOTA")
+# print("if I had $2000 as a reward, I would keep it for now.")
 ########################################################################
 # Task 1:
-
-
-
+# wallet = 10000
+# print(wallet + 50)
+# print(wallet)
+# print(4 ** 3)
 ########################################################################
 # Task 2:
+# animal1 = 'tiger'
+# animal2 = 'wolf'
+# animal3 = 'chicken'
+# print(animal1)
+# print(animal2)
+# print(animal3)
 
-
+# print(animal1 + animal2)
+# print(animal1 * 10)
 
 ########################################################################
 # Task 3:
-
+# variable1 = 3
+# variable2 = 5
+# print(variable1 + variable2)
+# print(variable2 * variable1)
+# print(variable1 / variable2)
 
 
 ########################################################################
 # Task 4:
 
-
-
+# name = 'Jacob'
+message = 'dot'
+# print(name + ' says ' + message)
 ########################################################################
 # Task 5:
-
+name = 'Jacob'
+# food = 'pizza'
+# print (name)
+# print ('likes to eat ' + food + str(message))
 
 
 ########################################################################
 # Task 6:
+# def intro(newfriend):
+#     print('Welcome')
+#     print('My name is ' + name)
+#     print('Nice to meet you ' + newfriend)
 
+# intro('friend')
+# intro('friend2')
 
 
 ########################################################################
 # Task 7:
+def addition(number1, number2):
+    print(number1 + number2)
 
+def subtraction(number1, number2):
+    print(number1 - number2)
+
+def division(number1, number2):
+    print(number1 / number2)
+
+def multiplication(number1, number2):
+    print(number1 * number2)
+
+multiplication(123, 456)
+
+division(99999999999999, 9)
+
+addition(20, 123)
+
+subtraction(2000, 1)
 
 
 ########################################################################
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -4,3 +4,72 @@
 
 
 ################## On Chat Commands Section #####################
+def teleport():
+    agent.teleport_to_player()
+
+player.on_chat("tp", teleport)
+
+def turnleft():
+    agent.turn(LEFT)
+player.on_chat("left", turnleft)
+
+
+def turnright():
+    agent.turn(RIGHT)
+player.on_chat("right", turnright)
+
+def moveforward(steps):
+    agent.move(FORWARD, steps)
+player.on_chat("forward", moveforward)
+
+def movebackwards(steps):
+    agent.move(BACK, steps)
+player.on_chat("backward", movebackwards)
+
+def attack():
+    agent.attack(FORWARD)
+player.on_chat("attack", attack)
+
+def jump(steps):
+    agent.move(UP, steps)
+player.on_chat("jump", jump)
+
+def down(steps):
+    agent.move(DOWN, steps)
+player.on_chat("down", down)
+
+def attackback():
+    agent.attack(BACK)
+player.on_chat("attackback", attackback)
+
+def destroyblock():
+    agent.destroy(FORWARD)
+player.on_chat("break", destroyblock)
+
+
+def MoveSteps():
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 6)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 3)
+    agent.turn_left()
+    agent.move(FORWARD, 4)
+    agent.turn_right()
+    agent.move(FORWARD, 4)
+
+player.on_chat("go", MoveSteps)
+
+def SPAWN():
+    player.teleport(pos(0, 0, 0))
+player.on_chat("spawn", SPAWN)
