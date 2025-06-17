b483ca0b6fe2f2d35d3213bf88f1c9750a702253
Computhink-CS137 <cs137@computhink.com.sg>
2025-06-17 02:22:25 +0000

    Jun 17, 2025, 2:22 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -51,7 +51,7 @@ for counter in range(50, 0,-1):
 # Additional exercises:
 
 def Newyearsprogram():
-    for counter in range(11, 0, -1):
+    for counter in range(10, 0, -1):
         print(counter)
     print('Happy New Year')
 
