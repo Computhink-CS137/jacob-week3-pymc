7c95e47c92672d652b860a18a968cf4c68f4b970
Computhink-CS137 <cs137@computhink.com.sg>
2025-06-18 03:09:00 +0000

    Jun 18, 2025, 3:09 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -41,7 +41,7 @@ print("hello from day3")
 ########################################################################
 # Task 5:
 # person1 = input('Whats his age?')
-#person
+# person1 +
 # person2 = input('Whats her age?')
 # if person1 > person2:
 #     print('Hes older')
