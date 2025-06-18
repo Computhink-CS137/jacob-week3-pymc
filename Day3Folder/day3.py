a2d316b97ba77e21695c5367be6c98b4bf78fdee
Computhink-CS137 <cs137@computhink.com.sg>
2025-06-18 02:32:51 +0000

    Jun 18, 2025, 2:32 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -25,7 +25,7 @@ print("hello from day3")
 # Task 3:
 number1 = input('Whats the 1st number?')
 number2 = input('Whats the 2nd number')
-results = number1
+results = int(number1)
 print(str(number1 + number2))
 
 
